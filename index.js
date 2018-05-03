b748d67015a8904a3a6049ea8e6b8d8f9b570148
fanyingmao <646900491@qq.com>
2018-05-03 17:50:30 +0800

    query提出

--- index.js
+++ index.js
@@ -7,20 +7,30 @@ class MongodbSql {
     if (typeof itme === 'object') {
       let valueKeys = Object.keys(itme);
       valueKeys.forEach(key2 => {
-        switch (key2) {
-          case '$in':
-            let arr = itme[key2];
-            values.push(...arr);
-            queryItem.push(key + ' in (' + arr.map(m => {
-              return '?'
-            }).join(',') + ')');
-            break;
-          case '$regex':
-            values.push(itme[key2]);
-            queryItem.push(key + ' regexp ' + '?');
-            break;
+        switch(key2) {
+        case
+          '$in'
+        :
+          let arr = itme[key2];
+          values.push(...arr
+        )
+          ;
+          queryItem.push(key + ' in (' + arr.map(m => {
+            return '?'
+          }).join(',') + ')'
+        )
+          ;
+          break;
+        case
+          '$regex'
+        :
+          values.push(itme[key2]);
+          queryItem.push(key + ' regexp ' + '?');
+          break;
         }
-      });
+      }
+    )
+      ;
     }
     else {
       values.push(itme);
@@ -28,6 +38,72 @@ class MongodbSql {
     }
     return {queryItem: queryItem.join(' and '), values: values};
   };
+
+  static dealQuery(query) {
+    let queryKeys = Object.keys(query);
+    let querySql;
+    let valueArr = [];//参数统一放入数组，防止sql注入
+    if (queryKeys.length === 0) {
+      querySql = '';
+    }
+    else {
+      querySql = ' where ';
+      let queryArr = [];
+      queryKeys.forEach(key => {
+        switch(key) {
+        case
+          '$or'
+        :
+          let $orQuery = [];
+          query[key].forEach(item => {
+            Object.keys(item).forEach(key2 => {
+            let {queryItem, values} = MongodbSql.dealItem(key2, item[key2]);
+          valueArr.push(...values
+        )
+          ;
+          $orQuery.push(queryItem);
+        })
+          ;
+        })
+          ;
+          if ($orQuery.length > 0) {
+            queryArr.push('(' + $orQuery.join(' or ') + ')');
+          }
+          break;
+        case
+          '$nor'
+        :
+          let $norQuery = [];
+          query[key].forEach(item => {
+            Object.keys(item).forEach(key2 => {
+            let {queryItem, values} = MongodbSql.dealItem(key2, item[key2]);
+          valueArr.push(...values
+        )
+          ;
+          $norQuery.push(queryItem);
+        })
+          ;
+        })
+          ;
+          if ($norQuery.length > 0) {
+            queryArr.push('!(' + $norQuery.join(' or ') + ')');
+          }
+          break;
+        default:
+          let {queryItem, values} = MongodbSql.dealItem(key, query[key]);
+          valueArr.push(...values
+        )
+          ;
+          queryArr.push(queryItem);
+          break;
+        }
+      }
+    )
+      ;
+      querySql += queryArr.join(' and ');
+    }
+    return {querySql, valueArr}
+  }
 
   /**
    * 生成sql查询语句
@@ -53,52 +129,7 @@ class MongodbSql {
       fieldSql = fieldsKeys.join(',');
     }
 
-    let queryKeys = Object.keys(query);
-    let querySql;
-    let valueArr = [];//参数统一放入数组，防止sql注入
-    if (queryKeys.length === 0) {
-      querySql = '';
-    }
-    else {
-      querySql = ' where ';
-      let queryArr = [];
-      queryKeys.forEach(key => {
-        switch (key) {
-          case '$or':
-            let $orQuery = [];
-            query[key].forEach(item => {
-              Object.keys(item).forEach(key2 => {
-                let {queryItem, values} = MongodbSql.dealItem(key2, item[key2]);
-                valueArr.push(...values);
-                $orQuery.push(queryItem);
-              });
-            });
-            if($orQuery.length > 0){
-              queryArr.push('(' + $orQuery.join(' or ') + ')');
-            }
-            break;
-          case '$nor':
-            let $norQuery = [];
-            query[key].forEach(item => {
-              Object.keys(item).forEach(key2 => {
-                let {queryItem, values} = MongodbSql.dealItem(key2, item[key2]);
-                valueArr.push(...values);
-                $norQuery.push(queryItem);
-              });
-            });
-            if($norQuery.length > 0){
-              queryArr.push('!(' + $norQuery.join(' or ') + ')');
-            }
-            break;
-          default:
-            let {queryItem, values} = MongodbSql.dealItem(key, query[key]);
-            valueArr.push(...values);
-            queryArr.push(queryItem);
-            break;
-        }
-      });
-      querySql += queryArr.join(' and ');
-    }
+    let {querySql, valueArr} = MongodbSql.dealQuery(query);
     if (!optSql) {
       optSql = '';
     }
